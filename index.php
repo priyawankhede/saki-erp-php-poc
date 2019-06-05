4ae5694452b981c6232c7971dc4fbe71b105d71d
pkwankhede <priyankawankhede1@yahoo.com>
2019-06-05 15:14:56 +0530

    changed

--- index.php
+++ index.php
@@ -14,7 +14,7 @@ if(!isset($_SESSION["email"])){
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=Edge">
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
-    <title>Saki Group</title>
+    <title>Saki Group Pune</title>
     <!-- Favicon-->
     <link rel="icon" href="favicon.ico" type="image/x-icon">
 
